7d1d504856301df043f80f43898cfb64dcec895b
jisoolee <szjslee@gmail.com>
2024-07-02 14:02:50 +0900

    ~pocketbase SDK 사용

---
+++ vite-auto/pocketbase/pb_migrations/1719883260_created_products.js
@@ -0,0 +1,105 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const collection = new Collection({
+    "id": "xz5u150i0zznz7s",
+    "created": "2024-07-02 01:21:00.156Z",
+    "updated": "2024-07-02 01:21:00.156Z",
+    "name": "products",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "nlprot8q",
+        "name": "brand",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "xglnusts",
+        "name": "description",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "ur8eg92y",
+        "name": "price",
+        "type": "number",
+        "required": true,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "noDecimal": false
+        }
+      },
+      {
+        "system": false,
+        "id": "hxktkzks",
+        "name": "ratio",
+        "type": "number",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "noDecimal": false
+        }
+      },
+      {
+        "system": false,
+        "id": "eaon0935",
+        "name": "photo",
+        "type": "file",
+        "required": true,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "mimeTypes": [
+            "image/jpeg",
+            "image/png",
+            "image/webp"
+          ],
+          "thumbs": [
+            "100x100"
+          ],
+          "maxSelect": 1,
+          "maxSize": 5242880,
+          "protected": false
+        }
+      }
+    ],
+    "indexes": [],
+    "listRule": null,
+    "viewRule": null,
+    "createRule": null,
+    "updateRule": null,
+    "deleteRule": null,
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("xz5u150i0zznz7s");
+
+  return dao.deleteCollection(collection);
+})
---
+++ vite-auto/src/pages/product/product.js
@@ -0,0 +1,47 @@
+import getPbImageURL from '@/api/getPbImageURL';
+import '@/pages/product/product.css';
+import gsap from 'gsap';
+import { comma, insertLast, setDocumentTitle } from 'kind-tiger';
+import pb from '@/api/pocketbase';
+
+// console.log(await pb.collection('products').getFullList());
+
+setDocumentTitle('29CM / 상품목록');
+
+async function renderProductItem() {
+  const productData = await pb.collection('products').getFullList({
+    sort: '-created',
+  });
+
+  /*  const response = await tiger.get(
+    `${import.meta.env.VITE_PB_API}/collections/products/records`
+  );
+
+  const productsData = response.data.items; */
+
+  productData.forEach((item) => {
+    const discount = item.price * (item.ratio * 0.01);
+
+    const template = `
+      <li class="product-item">
+          <div>
+            <figure>
+              <img src="${getPbImageURL(item)}" alt="" />
+            </figure>
+            <span class="brand">${item.brand}</span>
+            <span class="desc">${item.description}</span>
+            <span class="price">${comma(item.price)}원</span>
+            <div>
+              <span class="discount">${item.ratio}%</span>
+              <span class="real-price">${comma(item.price - discount)}원</span>
+            </div>
+          </div>
+        </li>
+    `;
+    insertLast('.container > ul', template);
+  });
+
+  gsap.from('.product-item', { y: 30, opacity: 0, stagger: 0.1 });
+}
+
+renderProductItem();
